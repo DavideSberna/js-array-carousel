932ce51dcf918445cee28a8847701e01d85d8921
DavideBS <104096563+DavideBS@users.noreply.github.com>
2023-03-14 17:18:09 +0100

    fetch index.js

--- js/index.js
+++ js/index.js
@@ -8,7 +8,8 @@ const arrImage = [
 
 const sliderRow = document.querySelector("#slider-row");
 const slider = document.querySelector("#slider-row div:nth-child(1)");
-// const sliderThumbs = document.querySelector("#slider-row div:nth-child(2)");
+const sliderThumbs = document.querySelector("#slider-row div:nth-child(2)");
+
 const next = document.querySelector(".next");
 const prev = document.querySelector(".prev");
 
@@ -22,14 +23,15 @@ for(let i = 0; i < arrImage.length; i++){
             <div class="card border-0 slider">
               <img src="${arrImage[i]}" alt="${i}">
             </div> `
-    // thumbs += `<div class="card border-0 slider thumbs-container active">
-    //              <img src="${arrImage[i]}" alt="${[i]}">
-    //            </div>`
+    thumbs += `<div class="card border-0 slider thumbs-container active">
+                 <img src="${arrImage[i]}" alt="${[i]}">
+               </div>`
 }
 slider.innerHTML += slide;
-// sliderThumbs.innerHTML += thumbs;
+sliderThumbs.innerHTML += thumbs;
+console.log(sliderThumbs)
 document.querySelectorAll(".slider")[currentIndex].classList.add("active");
-// document.querySelectorAll(".thumbs-container")[currentIndex].classList.add("border");
+document.querySelectorAll(".thumbs-container")[currentIndex].classList.add("border");
 
 
 
@@ -38,7 +40,7 @@ prev.addEventListener("click", prevSlide);
 
 function nextSlide(){
     document.querySelectorAll(".slider")[currentIndex].classList.remove("active");
-    // document.querySelectorAll(".thumbs-container")[currentIndex].classList.remove("border");
+    document.querySelectorAll(".thumbs-container")[currentIndex].classList.remove("border");
 
     if(currentIndex === arrImage.length - 1){
         currentIndex = 0;
@@ -46,18 +48,18 @@ function nextSlide(){
         currentIndex++;
     }
     document.querySelectorAll(".slider")[currentIndex].classList.add("active");
-    // document.querySelectorAll(".thumbs-container")[currentIndex].classList.add("border");
+    document.querySelectorAll(".thumbs-container")[currentIndex].classList.add("border");
 }
 function prevSlide(){
     document.querySelectorAll(".slider")[currentIndex].classList.remove("active");
-    // document.querySelectorAll(".thumbs-container")[currentIndex].classList.remove("border");
+    document.querySelectorAll(".thumbs-container")[currentIndex].classList.remove("border");
     if(currentIndex === 0){
         currentIndex = arrImage.length - 1;
     } else{
         currentIndex--;
     }
     document.querySelectorAll(".slider")[currentIndex].classList.add("active");
-    // document.querySelectorAll(".thumbs-container")[currentIndex].classList.add("border");
+    document.querySelectorAll(".thumbs-container")[currentIndex].classList.add("border");
 }
 
 
